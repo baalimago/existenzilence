fd99f60cbbae7fd9be0ca7868c3bc0d3d3f8ad17
goaway <goaway>
2021-02-28 14:34:47 +0100

    First implementation entierly done

--- content_scripts/existenzilence.js
+++ content_scripts/existenzilence.js
@@ -1,23 +1,72 @@
 (() => {
+
     if (window.hasRun) {
         return;
     }
     window.hasRun = true;
 
-    function hideSilencedComments(people) {
-        console.log("Hiding silenced people:");
-        console.log(people);
+    function hideSilencedComments(annoyingPeople) {
+        const comments = getAllUserComments();
+        for(const k in comments) {
+            const c = comments[k];
+            const n = c.children[1].innerText;
+            if(n in annoyingPeople) {
+                 c.style.display="none";
+            }
+        }
+    }
+
+    function getAllUserComments() {
+        const userComments = [];
+        const allBoxes = document.getElementsByClassName("box");
+        for (let i = 0; i< allBoxes.length; i++) {
+            if (allBoxes[i].id.includes("c")) {
+                userComments.push(allBoxes[i]);
+            }
+        }
+        return userComments;
     }
 
     function showSilencedComments() {
-        console.log("Reseting silence");
+       const comments = getAllUserComments();
+        for(const k in comments) {
+            const c = comments[k];
+            c.style.display="";
+        }
+    }
+
+    function loadFromLocalStorage() {
+        const storeStr = window.localStorage.getItem("annoyingPeople");
+        if (storeStr == null) return [];
+        userArr = storeStr.split(";");
+        const userMap = {};
+        for (const k in userArr) {
+            userMap[userArr[k]] = userArr[k];
+        }
+        return userMap;
+    }
+
+    function storeInLocService(annoyingPeople) {
+        let storeStr = "";
+        let i = 0;
+        for (const a in annoyingPeople) {
+            storeStr += annoyingPeople[a];
+            if (i < Object.keys(annoyingPeople).length - 1) storeStr += ";";
+            i++;
+        }
+        if (storeStr == "") return;
+        window.localStorage.setItem("annoyingPeople", storeStr);
     }
 
     browser.runtime.onMessage.addListener((message) => {
-        console.log(message);
         switch (message.command) {
             case "stfu":
                 hideSilencedComments(message.annoyingPeople);
+                storeInLocService(message.annoyingPeople);
+                break;
+
+            case "store":
+                storeInLocService(message.annoyingPeople);
                 break;
 
             case "oktalk":
@@ -28,5 +77,7 @@
                 break;
         }
     });
+
+    hideSilencedComments(loadFromLocalStorage());
 
 })();
